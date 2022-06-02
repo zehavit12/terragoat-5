733cece5ec074ccd81b48c79072790d4d42f22eb
zehavit12 <95243588+zehavit12@users.noreply.github.com>
2022-06-02 14:53:50 +0300

    Create testingpr.tf

---
+++ testingpr.tf
@@ -0,0 +1,21 @@
+resource "azurerm_managed_disk" "example" {
+  name                 = "terragoat-disk-${var.environment}"
+  location             = var.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 1
+  encryption_settings {
+    enabled = false
+  }
+  tags = {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/azure/storage.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "d17da7b3-f1c5-4723-9f77-d1b9069459c7"
+  }
+}
